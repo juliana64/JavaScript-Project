510a1b9734b3579f07505307e6e030aa163f343d
juliana64 <julianachelo@gmail.com>
2022-06-05 19:42:24 -0300

    beggening

---
+++ script.js
@@ -0,0 +1,12 @@
+function computerPlay(){
+    let rock = 'Rock';
+    let paper = 'Paper';
+    let scissors = 'Scissors';
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
