1c6d78f6cf849f7025e41c5d4ea2ef750cec7733
juliana64 <julianachelo@gmail.com>
2022-06-12 00:28:36 -0300

    game function

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
-function computerPlay(){
-    let rock = 'Rock';
-    let paper = 'Paper';
-    let scissors = 'Scissors';
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let draw = 0;
+let playRound = 1;
+
+function playerInput (){
+    let playerChoice = prompt ("Rock, Paper or Scissors?");
+    return playerChoice;
+    let playerSelection = playerChoice.toLocaleLowerCase();
+    return playerSelection;
 }
 
-function playRound(playerSelection, computerSelection){
-
+function getComputerChoice(){
+    return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function getWinner(){
+    if (playerSelection == computerSelection){
+        return draw;
+    }
+    else if (playerSelection == "rock"){
+        if (computerSelection == "paper"){
+            return 'computer';
+        }
+        else if (computerSelection == "scissors"){
+            return 'player';
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
+            return 'computer';
+        }
+        else if (computerSelection == "scissors"){
+            return 'player';
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "rock"){
+            return 'computer';
+        }
+        else if (computerSelection == 'paper'){
+            return 'player';
+        }
+    }
+    else{
+        return "You must have typed something wrong. Try again!";
+    }
+}
+
+function showWinner (winner){
+    if (winner == 'player'){
+        playerScore++ ;
+        return 'You Won!' + playerSelection.toUpperCase () + 'beats' + computerSelection.toUpperCase();
+    }
+    else if (winner == 'computer'){
+        computerScore++ ;
+        return 'You Lost' + computerSelection.toUpperCase () + 'beats' + playerSelection.toUpperCase();
+    }
+}
+
+function game(){
+    while (round <= 5){
+        playerSelection = playerInput ();
+        computerSelection = getComputerChoice();
+        let winner = getWinner ();
+        console.log(winner);
+        let score = showWinner (winner);
+        console.log(score);
+        round++;
+    }
+    console.log("Total Score: You:" + playerScore + "" + "Computer:" + computerScore + "" + "Draws:" + draw)
+}
+game();
